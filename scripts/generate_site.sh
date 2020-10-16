37b94737845f7a6401a1101fce8fb1ba382c9173
qingfengxia <qingfeng.xia@gmail.com>
2020-10-16 13:58:06 +0100

    update site_generation.py scripts doc and add generate_site.sh

--- scripts/generate_site.sh
+++ scripts/generate_site.sh
@@ -1,9 +1,15 @@
 # tested on Ubuntu 20.04
 # this script must be run in the build folder
+# github-ci run in project root,  assuming there exist 2 folders: doxygen
 
 MODULES="PPP Geom"
-MODULE_SOURCES="../PPP/* ../Geom/*"
+MODULE_SOURCES="../src/PPP/* ../src/Geom/*"
 REPO_DIR="../"
+
+if [ ! -d ../src ]; then
+echo "this script must be run in the build folder which must be a subfolder of repo root dir"
+exit 1
+fi
 
 # install tool if not yet installed
 #sudo apt install similarity-tester cppcheck flawfinder clang-tidy
@@ -18,7 +24,9 @@ make -j4 2>&1 | tee QA/clang_tidy_build_results.log
 
 cppcheck --language=c++ --std=c++17 --enable=all -DROOT_DIR=${REPO_DIR} -v --xml --xml-version=2 ${MODULES} 2>&1 | tee QA/cpp-check.xml
 
-sim_c++ -aR -o "QA/sim_cpp.log" ${MODULE_SOURCES}
+# -R recursively,  but can not control the depth, 
+# since third-party code moved into each module, remove -R option
+sim_c++ -a -o "QA/sim_cpp.log" ${MODULE_SOURCES}
 
 flawfinder ${MODULE_SOURCES} | tee QA/flawfinder.log
 
