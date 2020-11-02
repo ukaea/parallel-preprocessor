7887837acc3a8e1ef2d1f25b112f2bfeb8bae3b1
qingfengxia <qingfeng.xia@gmail.com>
2020-11-02 09:39:23 +0000

    update conda recipe after conda-forge built

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,18 +1,23 @@
+git submodule update --init --recursive
+
 mkdir build-conda
 pushd build-conda
 
+# this cause error in pybind11 cmake
+#    #-DPYTHON_EXECUTABLE=$PREFIX/bin/python \
+
 # for conda, pybind11 should detect the python installation correctly
 cmake \
+    -DPython3_EXECUTABLE="$PYTHON" \
     -DCMAKE_BUILD_TYPE=Release \
     -DCMAKE_INSTALL_PREFIX=$PREFIX \
-    -DPYTHON_EXECUTABLE=$PREFIX/bin/python \
-     ..
+    -DCMAKE_INSTALL_LIBDIR=$LIBRARY_LIB \
+    ..
 
 make -j4
 make install
 
-# python package built and installed to site-package by make install
-# ` build-conda/python` folder should have been copied/symlink to
-#python python/setup.py install --single-version-externally-managed --record record.txt
+# there is no need to run  setup.py in this repo
+# python package should has been built and installed to site-package by make install
 
 popd
